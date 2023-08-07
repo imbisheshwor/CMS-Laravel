9536bf5dce0c74fc2e7a144caf313b05a600d0a9
reshmapoudel <bkrazz2058@gmail.com>
2023-08-07 14:44:02 +0545

    updated

--- resources/views/dashboard/cpt/add.blade.php
+++ resources/views/dashboard/cpt/add.blade.php
@@ -14,7 +14,7 @@
                     <a href="{{ route('customPostType.index') }}" role="button" type="butt" on
                         class="focus:outline-none top-5 right-2 absolute text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">List</a>
                 </div>
-                <div class="flex-auto px-0 pt-0 pb-2">
+                {{-- <div class="flex-auto px-0 pt-0 pb-2">
                     <div class="p-0 overflow-x-auto  pl-10 mt-96">
 
                         <form action="{{ route('customPostType.store') }}" method="POST">
@@ -40,6 +40,33 @@
                                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button></a>
                         </form>
 
+
+                    </div>
+                </div> --}}
+
+                <div class="min-h-screen flex mt-20 justify-center">
+                    <div class="max-w-md w-1/2  p-6 bg-white rounded-lg shadow-lg">
+
+                        <h1 class="text-xl font-semibold text-center text-gray-500 mt-8 mb-6">Custom Post Type</h1>
+                        <form action="{{ route('customPostType.store') }}" method="POST">
+                            @csrf
+                            <div class="mb-4">
+                                <label for="nombre" class="block mb-2 text-sm text-gray-600">Name</label>
+                                <input type="text" id="name" name="name"
+                                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                                    required>
+                            </div>
+                            <div class="mb-4">
+                                <label for="apellido" class="block mb-2 text-sm text-gray-600">Slug</label>
+                                <input type="text" id="slug" name="slug"
+                                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                                    required>
+                            </div>
+
+
+                            <button type="submit"
+                                class="w-32 bg-gradient-to-r from-cyan-400 to-cyan-600 text-white py-2 rounded-lg mx-auto block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 mb-2">Submit</button>
+                        </form>
 
                     </div>
                 </div>
--- resources/views/dashboard/layout/head.blade.php
+++ resources/views/dashboard/layout/head.blade.php
@@ -18,8 +18,9 @@
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <!-- Main Styling -->
     <link href="{{ asset('assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5') }}" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 
-   
+
 </head>
